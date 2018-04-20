61abca05997fd35795f3378d5ba1f328a4b7f215
James Scott-Brown <james@jamesscottbrown.com>
2018-04-20 17:50:03 +0100

    tidy examples/bellshape-complete.py

--- examples/bellshape-complete.py
+++ examples/bellshape-complete.py
@@ -5,7 +5,7 @@ from sympy import init_printing, Matrix, transpose, pprint
 from numpy import savetxt
 from numpy import linspace
 
-def formCRN():
+def form_crn():
     K = Species('K', initial_value=0.1)
     A = Species('A')
     B = Species('B')
@@ -26,60 +26,74 @@ def formCRN():
 
     return CRNSketch([reaction1, reaction2], [reaction3], [])
 
-derivatives = [{"variable": 'K', "order": 1, "is_variance": False, "name": "dK_dt"}]
-# specification = [('', 'dK_dt >= 0', '((K > 0) and (dK_dt = 0))'), ('', 'dK_dt <= 0', '((K >= 0) and (K < 0.1))')]
-# specification = [('', '(dK_dt >= 0)', '(dK_dt = 0)' ), ('', '(dK_dt < 0)', '(dK_dt < 0)')]
-specification = [('', '', '(K > 0.1)'), ('', '', '(K < 0.3)')]
+def synthesize_with_isat(crn):
 
-crn = formCRN()
-flow = crn.flow(False, derivatives)
-hys = iSATParser.constructISAT(crn, specification, flow)
-with open('bellshape.hys', 'w') as file:
-    file.write(hys)
+    derivatives = [{"variable": 'K', "order": 1, "is_variance": False, "name": "dK_dt"}]
+    # specification = [('', 'dK_dt >= 0', '((K > 0) and (dK_dt = 0))'), ('', 'dK_dt <= 0', '((K >= 0) and (K < 0.1))')]
+    # specification = [('', '(dK_dt >= 0)', '(dK_dt = 0)' ), ('', '(dK_dt < 0)', '(dK_dt < 0)')]
+    specification = [('', '', '(K > 0.1)'), ('', '', '(K < 0.3)')]
 
-specification_dreal = [('K < 0.2', 'dK_dt >= 0', '(and (K > 0.3) (dK_dt = 0))'), ('', 'dK_dt <= 0', '(and (K >= 0)(K < 0.1))')]
-drh = iSATParser.constructdReal(crn, specification_dreal, flow)
-with open('bellshape.drh', 'w') as file:
-    file.write(drh)
+    flow = crn.flow(False, derivatives)
+    hys = iSATParser.constructISAT(crn, specification, flow)
+    with open('bellshape.hys', 'w') as file:
+        file.write(hys)
 
 
-# # Try to solve using iSAT
-# sc = SolverCallerISAT("./bellshape.hys", isat_path="../isat-ode-r2806-static-x86_64-generic-noSSE-stripped.txt")
-#
-# result_files = sc.single_synthesis(cost=0)
-#
-# for file_name in result_files:
-#     print("\n\n")
-#     # print(sc.getCRNValues(file_name))
-#
-#     vals, all_vals = sc.getCRNValues(file_name)
-#     initial_conditions, parametrised_flow = sc.get_full_solution(crn, flow, all_vals)
-#
-#     print("Initial Conditions", initial_conditions)
-#     print("Flow:", parametrised_flow)
-#
-#
-#     t, sol, variable_names = sc.simulate_solutions(initial_conditions, parametrised_flow,
-#                                                    plot_name=file_name + "-simulation.png")
-#     print("\n\n")
-#     print(variable_names)
-#     print(sol)
-#     savetxt(file_name + "-simulation.csv", sol, delimiter=",")
+    t = linspace(0, 50, 100)
 
+    sc = SolverCallerISAT("./bellshape.hys", isat_path="../isat-ode-r2806-static-x86_64-generic-noSSE-stripped.txt")
 
-sc = SolverCallerDReal("./bellshape.drh", dreal_path="../dReal-3.16.09.01-linux/bin/dReach")
-result_files = sc.single_synthesis(cost=0)
+    result_files = sc.single_synthesis(cost=0)
 
+    for file_name in result_files:
+        print("\n\n")
+        # print(sc.getCRNValues(file_name))
 
-for file_name in result_files:
-        vals, all_vals = sc.getCRNValues('./bellshape_1_0.smt2.proof')
+        vals, all_vals = sc.getCRNValues(file_name)
         initial_conditions, parametrised_flow = sc.get_full_solution(crn, flow, all_vals)
 
         print("Initial Conditions", initial_conditions)
         print("Flow:", parametrised_flow)
+
         t, sol, variable_names = sc.simulate_solutions(initial_conditions, parametrised_flow,
-                                                       plot_name=file_name + "-simulationdreal.png", t = linspace(0, 100, 1000))
+                                                       plot_name=file_name + "-simulation.png")
         print("\n\n")
         print(variable_names)
         print(sol)
-        savetxt(file_name + "-simulationdreal.csv", sol, delimiter=",")
+        savetxt(file_name + "-simulation.csv", sol, delimiter=",")
+
+
+def synthesize_with_dreal(crn):
+    derivatives = [{"variable": 'K', "order": 1, "is_variance": False, "name": "dK_dt"}]
+    flow = crn.flow(False, derivatives)
+
+    specification_dreal = [('K < 0.2', 'dK_dt >= 0', '(and (K > 0.3) (dK_dt = 0))'),
+                           ('', 'dK_dt <= 0', '(and (K >= 0)(K < 0.1))')]
+
+    drh = iSATParser.constructdReal(crn, specification_dreal, flow)
+    with open('bellshape.drh', 'w') as file:
+        file.write(drh)
+
+    sc = SolverCallerDReal("./bellshape.drh", dreal_path="../dReal-3.16.09.01-linux/bin/dReach")
+    result_files = sc.single_synthesis(cost=0)
+
+
+    for file_name in result_files:
+            vals, all_vals = sc.getCRNValues('./bellshape_1_0.smt2.proof')
+            initial_conditions, parametrised_flow = sc.get_full_solution(crn, flow, all_vals)
+
+            print("Initial Conditions", initial_conditions)
+            print("Flow:", parametrised_flow)
+            t, sol, variable_names = sc.simulate_solutions(initial_conditions, parametrised_flow,
+                                                           plot_name=file_name + "-simulationdreal.png", t = linspace(0, 100, 1000))
+            print("\n\n")
+            print(variable_names)
+            print(sol)
+            savetxt(file_name + "-simulationdreal.csv", sol, delimiter=",")
+
+
+if __name__ == "__main__":
+    crn = form_crn()
+
+    synthesize_with_isat(crn)
+    synthesize_with_dreal(crn)
