cb188e63d5972a80597194d1afb796848262a2aa
max1s <max.whitby@cs.ox.ac.uk>
2018-06-29 15:20:28 +0100

    tidy up examples/

--- examples/toggle-switch/toggleswitch.py
+++ examples/toggle-switch/toggleswitch.py
@@ -3,29 +3,30 @@ from CRNSynthesis import iSATParser
 from CRNSynthesis.solverCaller import SolverCallerISAT, SolverCallerDReal
 from sympy import init_printing, Matrix, transpose, pprint
 from numpy import savetxt
-
+from numpy import linspace
 
 def form_crn():
+    rate = "-(-50 + t)/(50 * exp((-50 + t)**2/100))"
 
     lacI = Species('lacI')
     cIts = Species('cIts')
     lacIStar = Species('lacIStar')
-    IPTG = Species('IPTG')
+    # IPTG = Species('IPTG')
 
-    input1 = InputSpecies("Input1", sympify("(-355.556 * exp(-177.778 * (-0.5 + t) ** 2) * (-0.5 + t))"), initial_value=0.01)
+    input1 = InputSpecies("Input1", sympify(rate), initial_value=0.01)
 
-    k_1 = RateConstant('k_1', 0.1, 2)
-    k_2 = RateConstant('k_2', 0.1, 2)
+    k_1 = RateConstant('k_1', 0.2, 2)
+    k_2 = RateConstant('k_2', 0.2, 2)
 
-    alpha_1 = RateConstant('alpha_1', 0.1, 2)
-    alpha_2 = RateConstant('alpha_2', 0.1, 2)
+    alpha_1 = RateConstant('alpha_1', 0.2, 2)
+    alpha_2 = RateConstant('alpha_2', 0.2, 2)
 
-    degredation_1 = RateConstant('degredation_1', 0.1, 2)
-    degredation_2 = RateConstant('degredation_2', 0.1, 2)
+    degredation_1 = RateConstant('degredation_1', 0.2, 2)
+    degredation_2 = RateConstant('degredation_2', 0.2, 2)
 
     Ka = RateConstant('Ka', 1, 1)
 
-    reaction1 = ArbitraryRateReaction([Term(input1, 1)], [Term(lacI, 1), Term(input1, 1)], 'alpha_2 / (1 + ((k_2 * lacI)/ (k_1 * IPTG) + k_2) ** 2)', [alpha_2, k_1, k_2])
+    reaction1 = ArbitraryRateReaction([Term(input1, 1)], [Term(lacI, 1), Term(input1, 1)], 'alpha_2 / (1 + ((k_2 * lacI)/ (k_1 * Input1) + k_2) ** 2)', [alpha_2, k_1, k_2])
     reaction2 = HillRepressionReaction([Term(lacI, 1)], [Term(lacI, 1),Term(cIts, 1)], alpha_1, Ka, 2)
 
     reaction3 = Reaction([Term(lacI, 1)], [], degredation_1)
@@ -37,18 +38,19 @@ def form_crn():
 def synthesize_with_isat(crn):
     isLNA = False
     derivatives = []
-    specification = []
+    #derivatives = [{"variable": 'cIts', "order": 1, "is_variance": False, "name": "dcIts"}]
+    specification = [('','','cIts > 0.3'),('','','(cIts < 0.3) and (time = MAX_TIME)')]
 
     flow = crn.flow(isLNA, derivatives)
 
-    hys = iSATParser.constructISAT(crn, specification, flow)
+    hys = iSATParser.constructISAT(crn, specification, flow, max_time=100, other_constraints='(cIts < 0.1) and (time = 0);',scale_factor=1)
 
     with open("./toggleswitch.hys", "w") as f:
         f.write(hys)
 
     sc = SolverCallerISAT("./toggleswitch.hys", isat_path="../isat-ode-r2806-static-x86_64-generic-noSSE-stripped.txt")
 
-    result_files = sc.single_synthesis(cost=0)
+    result_files = sc.single_synthesis(cost=0, precision=0.01, msw=0.1)
 
     for file_name in result_files:
         vals, all_vals = sc.getCRNValues(file_name)
@@ -57,7 +59,8 @@ def synthesize_with_isat(crn):
         print("\n\nInitial Conditions", initial_conditions)
         print("Flow:", parametrised_flow)
 
-        t, sol, variable_names = sc.simulate_solutions(initial_conditions, parametrised_flow)
+        t, sol, variable_names = sc.simulate_solutions(initial_conditions, parametrised_flow,
+                                                       plot_name=file_name + "-simulation.png", t = linspace(0, 100, 1000))
         print("\n\n")
         print(variable_names)
         print(sol)
