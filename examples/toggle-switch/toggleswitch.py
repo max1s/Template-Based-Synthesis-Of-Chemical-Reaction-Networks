23815e01d9b80905499afbe563df902de2dfeed2
max1s <max.whitby@cs.ox.ac.uk>
2018-07-09 11:59:32 +0100

    Toggle Switch

--- examples/toggle-switch/toggleswitch.py
+++ examples/toggle-switch/toggleswitch.py
@@ -94,7 +94,7 @@ def synthesize_with_dreal(crn):
             print("Initial Conditions", initial_conditions)
             print("Flow:", parametrised_flow)
             t, sol, variable_names = sc.simulate_solutions(initial_conditions, parametrised_flow,
-                                                           plot_name=file_name + "-simulationdreal.png", t = linspace(0, 10, 100))
+                                                           plot_name=file_name + "-simulationdreal.png", t = linspace(0, 100, 100))
             print("\n\n")
             print(variable_names)
             print(sol)
